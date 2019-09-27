49f036e0b9b87fc1d0d8b208cdb91a769497aeb0
Jingguo Yao <yaojingguo@gmail.com>
2019-09-27 17:47:55 +0800

    update

--- src/test/java/org/yao/jute/JuteTests.java
+++ src/test/java/org/yao/jute/JuteTests.java
@@ -13,10 +13,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.TreeMap;
 
 public class JuteTests {
